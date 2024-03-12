a403b7c9980d1819f81c4a11bfcf66b259c530a5
Greg Dennis <gregsdennis@yahoo.com>
2024-03-13 11:38:45 +1300

    add example app

---
+++ MyBlazorApp/MyLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace MyLibrary;
+
+public static class MyService
+{
+	public static string GetText(CultureInfo culture)
+	{
+		return new ResourceManager("MyLibrary.Resources.MyLibrary", typeof(MyService).Assembly).GetString("HelloWorld", culture);
+	}
+}
